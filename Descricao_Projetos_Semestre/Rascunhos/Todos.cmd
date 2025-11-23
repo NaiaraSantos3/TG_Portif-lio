5b80b0357812be6717ae994c727a1c1bc20b5c50
Naiara Santos <nai.leticialemos31@gmail.com>
2025-11-23 20:07:34 -0300

    docs: ajuste/correção de estrutura

---
+++ Descricao_Projetos_Semestre/Rascunhos/Todos.cmd
@@ -0,0 +1,42 @@
+# Apresentação de Portifólio: Naiara Santos
+
+## Sumário
+- [Introdução](#introdução)
+- [Contatos](#contatos)
+- [Meus Principais Conhecimentos](#meus-principais-conhecimentos)
+- [Meus Projetos](#meus-projetos)
+
+
+
+## Introdução
+<details> 
+"Faça uma breve introdução sobre você. Fale de sua formação e carreira. Caso aplicável, fale de publicações ou apresentações em eventos da área de computação. Inclua uma foto!"
+</details>
+
+## Contatos
+<details> 
+
+* [GIT](https://github.com/NaiaraSantos3)
+* [LinkedIn](https://www.linkedin.com/in/naiara-santos-73b83a186/)
+</details>
+
+## Meus Principais Conhecimentos
+<details> 
+
+"Apresente seus principais conhecimentos. Foque nos conhecimentos que possui maior domínio e que deseja desenvolver durante sua carreira."
+</details>
+
+
+## Meus Projetos
+<details> 
+
+Durante a realização do curso de Banco de Dados na Fatec - Faculdade de Tecnologia de São José dos Campos - Profº Jessen Vidal, tive a interação com a Metodologia de Aprendizado - API e em todos os semestres foram desenvolvidos ao menos um projeto em equipe e com base na metodologia de Scrum.
+Neste portifólio, serão apresentadas os parceiros (clientes), meus aprendizados, contribuições pessoais com os times que trabalhei e as soluções elaboradas em cada um dos semestres. 
+</details>
+
+- [1º Semestre](Sem_1/README.md)
+- [2º Semestre](Sem_2/README.md)
+- [3º Semestre](Sem_3/README.md)
+- [4º Semestre](Sem_4/README.md)
+- [5º Semestre](Sem_5/README.md)
+- [6º Semestre](Sem_6/README.md)
